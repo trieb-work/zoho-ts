9a3bea4ad030461b64e6263eab0c934342cfc431
Jannik Zinkl <jannik@trieb.work>
2021-05-04 14:37:36 +0200

    first release on public repo

---
+++ README.md
@@ -0,0 +1,15 @@
+# Zoho Inventory TS
+A typescript library used to work with Zoho Inventory and Zoho Books API.
+There are several functions already created, but in the end they are all custom. Some of them are documented and some are reverse-engineered and implemented in this library.
+
+## Usage
+```
+import { ZohoClientInstance } from '@trieb.work/zoho-inventory-ts';
+
+
+const zohoClient = new ZohoClientInstance({
+            zohoClientId: '',
+            zohoClientSecret: '',
+            zohoOrgId: '',
+        });
+```
---
+++ src/test.index.ts
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+import { ZohoClientInstance } from './index';
+
+dotenv.config({ path: '../../.env' });
+
+async function main() {
+    const client = new ZohoClientInstance({
+        zohoClientId: process.env.ZOHO_CLIENT_ID,
+        zohoClientSecret: process.env.ZOHO_CLIENT_SECRET,
+        zohoOrgId: process.env.ZOHO_ORGANIZATION_ID,
+    });
+
+    try {
+        await client.authenticate();
+        console.log(await client.salesOrderEditpage())
+        const id = await client.getContact({
+            first_name: 'Jannik',
+            last_name: 'Zinkl',
+            email: 'zinkljannik@gmail.com',
+            company_name: '',
+        });
+        console.log('id', id);
+
+        console.log(await client.getPackagesTotal({ from: '2021-01-01', to: '2021-02-01' }));
+        console.log(await client.getContactById(id));
+        console.log(await client.getSalesorder('SO-000100'))
+    } catch (error) {
+        console.error(error);
+    }
+}
+main();
