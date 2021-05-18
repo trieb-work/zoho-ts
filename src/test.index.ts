784ba4c651e469b7a07d17d0a1ddbd801f49a012
Jannik Zinkl <jannik@trieb.work>
2021-05-18 17:27:31 +0200

    test improvements

--- src/test.index.ts
+++ src/test.index.ts
@@ -12,7 +12,7 @@ async function main() {
 
     try {
         await client.authenticate();
-        console.log(await client.salesOrderEditpage())
+        console.log(await client.salesOrderEditpage());
         const id = await client.getContact({
             first_name: 'Jannik',
             last_name: 'Zinkl',
@@ -23,7 +23,9 @@ async function main() {
 
         console.log(await client.getPackagesTotal({ from: '2021-01-01', to: '2021-02-01' }));
         console.log(await client.getContactById(id));
-        console.log(await client.getSalesorder('SO-000100'))
+        console.log(await client.getSalesorder('SO-000100'));
+
+        console.log(await client.getContactWithFullAdresses('116240000000351141'));
     } catch (error) {
         console.error(error);
     }
